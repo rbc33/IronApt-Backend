e00ee952caa38a025955a307fb5bc8be1ee12eb1
rbc33 <ricardobenthem@gmail.com>
2026-01-30 19:51:34 +0100

    cors

--- config/index.ts
+++ config/index.ts
@@ -25,8 +25,13 @@ const middlewareConfig = (app: Application) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      // origin: [FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
-      origin: "*",
+      origin: [
+        FRONTEND_URL,
+        "https://proyect3-frontend.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+      ],
+      credentials: true,
     }),
   );
 
