23ae01669af02875f15d9291dd26629402bd7b03
rbc33 <ricardobenthem@gmail.com>
2026-01-30 18:38:49 +0100

    git push cors *

--- config/index.ts
+++ config/index.ts
@@ -25,7 +25,8 @@ const middlewareConfig = (app: Application) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+      // origin: [FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+      origin: "*",
     }),
   );
 
