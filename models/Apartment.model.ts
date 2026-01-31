c9bb155b889cd84c1b37c26c7535f315a18bc5e5
rbc33 <ricardobenthem@gmail.com>
2026-01-31 19:16:57 +0100

    set to images, showing one

--- models/Apartment.model.ts
+++ models/Apartment.model.ts
@@ -1,16 +1,15 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const apartmentSchema = new Schema({
-    name: { type: String, required: true },
-    image: [{ type: String, required: true }],
-    size: { type: Number, required: true },
-    pricePerDay: { type: Number, required: true },
-    description: { type: String, required: true },
-    capacity: { type: Number, required: true },
-    bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
-  });
+  name: { type: String, required: true },
+  images: [{ type: String, required: true }],
+  size: { type: Number, required: true },
+  pricePerDay: { type: Number, required: true },
+  description: { type: String, required: true },
+  capacity: { type: Number, required: true },
+  bookings: [{ type: Schema.Types.ObjectId, ref: "Booking" }],
+});
 
-  const Apartment = model('Apartment', apartmentSchema);
+const Apartment = model("Apartment", apartmentSchema);
 
-  export default Apartment;
-
+export default Apartment;
--- routes/apartment.routes.ts
+++ routes/apartment.routes.ts
@@ -6,11 +6,11 @@ import { isAuthenticated } from "../middleware/jwt.middleware.js";
 const router = Router();
 
 router.post("/", (req, res, next) => {
-  const { name, image, size, pricePerDay, description, capacity } = req.body;
+  const { name, images, size, pricePerDay, description, capacity } = req.body;
 
   const newApartment = new Apartment({
     name,
-    image,
+    images,
     size,
     pricePerDay,
     description,
@@ -30,23 +30,40 @@ router.get("/", (req, res, next) => {
     .catch((err) => next(err));
 });
 
-router.get("/:id",isAuthenticated, (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
-  // console.log(req)
-  Apartment.findById(id)
-    .populate("bookings")
-    .then((apartments) => res.status(200).json(apartments))
-    .catch((err) => next(err));
-});
+router.get(
+  "/:id",
+  isAuthenticated,
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    // console.log(req)
+    Apartment.findById(id)
+      .populate("bookings")
+      .then((apartments) => res.status(200).json(apartments))
+      .catch((err) => next(err));
+  },
+);
 
-router.put("/:id",isAuthenticated, (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
-  const { _id, name, image, size, pricePerDay, description, capacity } = req.body;
-  const updatedApt = {_id, name, image, size, pricePerDay, description, capacity }
-  Apartment.findByIdAndUpdate(id, updatedApt, {new: true})
-    .populate("bookings")
-    .then((apartments) => res.status(200).json(apartments))
-    .catch((err) => next(err));
-});
+router.put(
+  "/:id",
+  isAuthenticated,
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const { _id, name, images, size, pricePerDay, description, capacity } =
+      req.body;
+    const updatedApt = {
+      _id,
+      name,
+      images,
+      size,
+      pricePerDay,
+      description,
+      capacity,
+    };
+    Apartment.findByIdAndUpdate(id, updatedApt, { new: true })
+      .populate("bookings")
+      .then((apartments) => res.status(200).json(apartments))
+      .catch((err) => next(err));
+  },
+);
 
 export default router;
